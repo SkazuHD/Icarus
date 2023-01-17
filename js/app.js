66c2785f9409b439d4b77ac9c868441e9c7763fd
SkazuHD <81975854+SkazuHD@users.noreply.github.com>
2023-01-17 19:28:57 +0100

    A lot

--- js/app.js
+++ js/app.js
@@ -1,18 +1,17 @@
 let heroText = document.getElementById("super");
+let cpYear = document.getElementById("cp-year");
 
-let texte = [
-  "Lerne zu Fliegen !",
-  "The Future is in the air !",
-  "#LearnToFly",
-  "Hol dir die Freiheit !",
-];
+let currentYear = new Date().getFullYear();
+cpYear.innerText = currentYear;
+
+let texte = ["Lerne zu Fliegen !", "The Future is in the air !", "#LearnToFly", "Hol dir die Freiheit !"];
 let index = 0;
 setInterval(function () {
   heroText.classList.add("fadeout");
   setTimeout(function () {
     heroText.innerText = texte[index];
     heroText.classList.remove("fadeout");
-  }, "3000");
+  }, "2000");
   index++;
   if (index == texte.length) {
     index = 0;
